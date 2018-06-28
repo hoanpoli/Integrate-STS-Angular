8f1fefac61ffaf44d358e8b1860115c7fe5fff51
Hoan Nguyen <nvhoanbk14@gmail.com>
2018-06-28 11:46:36 +0700

    update routing server and fix format code in db file

--- src/main/resources/db/migration/V1_01_02__AddData.sql
+++ src/main/resources/db/migration/V1_01_02__AddData.sql
@@ -1,6 +1,6 @@
-
-INSERT INTO "account"("id","last_name","first_name") VALUES ('1','Lig1','Ligue 1');
-INSERT INTO "account"("id","last_name","first_name") VALUES ('2','THT','Thắng Hòa Thua');
-INSERT INTO "account"("id","last_name","first_name") VALUES ('3','TS','Tỷ số');
-INSERT INTO "account"("id","last_name","first_name") VALUES ('4','TX','Tài xỉu');
-INSERT INTO "account"("id","last_name","first_name") VALUES ('5','DVD','Đội vô địch');
+INSERT INTO "account" ("id","last_name","first_name") VALUES
+	('1', 'Lig1', 'Ligue 1'),
+	('2', 'THT', 'Thắng Hòa Thua'),
+	('3', 'TS', 'Tỷ số'),
+	('4', 'TX', 'Tài xỉu'),
+	('5', 'DVD', 'Đội vô địch');
