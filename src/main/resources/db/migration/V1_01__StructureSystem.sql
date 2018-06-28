5e5b7e614842cda795c33f48fbe6d6210ae6b7f2
Hoan Nguyen <nvhoanbk14@gmail.com>
2018-06-28 08:03:48 +0700

    add file create database

---
+++ src/main/resources/db/migration/V1_01__StructureSystem.sql
@@ -0,0 +1,10 @@
+-- Table: public."account"
+
+-- DROP TABLE public."account";
+DROP TABLE IF EXISTS PUBLIC."account";
+CREATE TABLE "account"
+(
+	"id"					SERIAL PRIMARY KEY,
+	"last_name"					VARCHAR(64),
+	"first_name"					VARCHAR(64)	
+);
